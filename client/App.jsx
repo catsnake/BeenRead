3b9de1177d9d720f3acbd98b71c9b80f4e792f04
punkygreg <guigui.v1999@gmail.com>
2024-02-24 12:24:08 -0500

    login and signup

--- client/App.jsx
+++ client/App.jsx
@@ -4,11 +4,14 @@ import { Router, Route, Routes, Link, BrowserRouter } from 'react-router-dom';
 import Authenticator from './components/Authenticator.jsx';
 import Feed from './components/Feed.jsx';
 
+import styles from './styles.scss';
 
 
 const App = () => {
   return (
-    <div>
+    <div><h1 id= 'title'>
+        BeRead : Expand Your Mind
+    </h1>
       <Authenticator />
     </div>
   );
---
+++ client/slices/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./api/apiSlice";
+import authReducer from "./reducers/authSlice";
+
+
+export const store = configureStore({
+  reducer: {
+    // We can reuse the apiSlice.reducerPath field as a computed key in the reducer parameter, to ensure that the caching reducer is added in the right place.
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
+});
