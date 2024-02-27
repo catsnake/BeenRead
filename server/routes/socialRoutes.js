f08c4ac826e18dbbecbd5f427481b8d2afb95c57
Charlie Schubach <charlie.schubach@gmail.com>
2024-02-27 16:19:09 -0500

    const

--- server/routes/socialRoutes.js
+++ server/routes/socialRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const socialController = require('../controllers/socialController');
 
-router = express.Router();
+const router = express.Router();
 
 router.post('/followUser', socialController.followUser, (req, res) => {
   res.status(200).json(res.locals.user);
