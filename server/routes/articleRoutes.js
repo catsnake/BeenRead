b94a332f7f925c56639b94e782fa752f67d275a6
Branden Basche <brandenbasche@gmail.com>
2024-02-27 20:02:33 -0500

    pushing to pull from dev

--- server/routes/articleRoutes.js
+++ server/routes/articleRoutes.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const openAiController = require("../controllers/openAiController");
-const articleController = require("../controllers/articleController");
+const express = require('express');
+const openAiController = require('../controllers/openAiController');
+const articleController = require('../controllers/articleController');
 
 const router = express.Router();
 
 router.post(
-  "/saveArticle",
+  '/saveArticle',
   openAiController.getArticle,
   articleController.saveArticle,
   (req, res) => {
     res.status(201).json(res.locals.savedArticle);
-  }
+  },
 );
 
 router.get(
-  "/generateArticleHistory/:id",
+  '/generateArticleHistory/:id',
   articleController.generateArticleHistory,
   (req, res) => {
     res.status(200).json(res.locals.generateArticleHistory);
-  }
+  },
 );
 
-router.put( '/checkIsRead', articleController.checkIsRead, (req, res) => {
-  res.status(200).json(res.locals.findArticle)
-})
+router.put('/checkIsRead', articleController.checkIsRead, (req, res) => {
+  res.status(200).json(res.locals.findArticle);
+});
 
 module.exports = router;
