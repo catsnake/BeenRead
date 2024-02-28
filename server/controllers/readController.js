4a1a01a96ce63201acf67f2aa6d39448172581b4
Charlie Schubach <charlie.schubach@gmail.com>
2024-02-28 11:30:14 -0500

    reading controllers

--- server/controllers/readController.js
+++ server/controllers/readController.js
@@ -3,28 +3,171 @@ const User = require('../models/userModel');
 const readController = {};
 
 readController.readDailyArticle = async (req, res, next) => {
-//change 
+  try {
+    const { username } = req.params;
 
-// import username from params
+    const user = await User.findOne({ username });
 
-// change boolean to true for db r
+    if (!user) {
+      return next({
+        log: 'Error in readController.readDailyArticle: ',
+        message: { error: 'User not found' },
+      });
+    }
+
+    const userId = user._id;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { $set: { 'feed.readDailyArticle': true } },
+      { new: true }
+    );
+
+    res.locals.user = updatedUser;
+
+    return next();
+  } catch (error) {
+    return next({
+      log: 'Error in readController.readDailyArticle: ',
+      message: { error: 'Cannot read daily article' },
+    });
+  }
 };
 
-readController.updateTimeStarted = async (req, res, next) => {
+readController.updateTimeFinished = async (req, res, next) => {
+  try {
+    const { username } = req.params;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return next({
+        log: 'Error in readController.updateTimeStarted: ',
+        message: { error: 'User not found' },
+      });
+    }
 
+    const userId = user._id;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { $set: { 'feed.timeFinishedReading': Date.now() } },
+      { new: true }
+    );
+
+    res.locals.user = updatedUser;
+
+    return next();
+  } catch (error) {
+    return next({
+      log: 'Error in readController.updateTimeStarted: ',
+      message: { error: 'Cannot update time started' },
+    });
+  }
 };
 
 readController.updateTimeSpent = async (req, res, next) => {
+  try {
+    const { username, timeSpent } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return next({
+        log: 'Error in readController.updateTimeSpent: ',
+        message: { error: 'User not found' },
+      });
+    }
 
+    const userId = user._id;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { $set: { 'feed.timeSpentReading': timeSpent } },
+      { new: true }
+    );
+
+    res.locals.user = updatedUser;
+
+    return next();
+  } catch (error) {
+    return next({
+      log: 'Error in readController.updateTimeSpent: ',
+      message: { error: 'Cannot update time spent' },
+    });
+  }
 };
 
 readController.updateDailyStreak = async (req, res, next) => {
+  try {
+    const { username } = req.params;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return next({
+        log: 'Error in readController.updateDailyStreak: ',
+        message: { error: 'User not found' },
+      });
+    }
 
+    const userId = user._id;
+    const newStreak = user.feed.dailyStreak + 1;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { $set: { 'feed.dailyStreak': newStreak } },
+      { new: true }
+    );
+
+    res.locals.user = updatedUser;
+
+    return next();
+  } catch (error) {
+    return next({
+      log: 'Error in readController.updateDailyStreak: ',
+      message: { error: 'Cannot update daily streak' },
+    });
+  }
 };
 
 readController.dailyReset = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    if (!users) {
+      return next({
+        log: 'Error in readController.dailyReset: ',
+        message: { error: 'No users found' },
+      });
+    }
 
+    for (const user of users) {
+      let userId = user._id;
+      const readDailyArticle = user.feed.readDailyArticle;
+
+      if (!readDailyArticle) {
+        await User.findByIdAndUpdate(userId, {
+          $set: { 'feed.dailyStreak': 0 },
+        });
+      }
+
+      let updatedUser = await User.findByIdAndUpdate(
+        userId,
+        {
+          $set: {
+            'feed.readDailyArticle': false,
+            'feed.timeFinishedReading': null,
+            'feed.timeSpentReading': 0,
+            'feed.dailyReactions': [],
+          },
+        },
+        { new: true }
+      );
+
+      res.locals.users[userId] = updatedUser;
+    }
+
+    return next();
+  } catch (error) {
+    return next({
+      log: 'Error in readController.dailyReset: ',
+      message: { error: 'Cannot reset daily streak' },
+    });
+  }
 };
-
 
 module.exports = readController;
--- server/routes/readRoutes.js
+++ server/routes/readRoutes.js
@@ -3,6 +3,24 @@ const readController = require('../controllers/readController');
 
 const router = express.Router();
 
+router.get('/readDailyUser', socialController.readDailyUser, (req, res) => {
+    res.status(200).json(res.locals.user);
+  });
 
+router.get('/updateTimeFinished', socialController.updateTimeFinished, (req, res) => {
+    res.status(200).json(res.locals.user);
+});
+
+router.post('/updateTimeSpent', socialController.updateTimeSpent, (req, res) => {
+    res.status(200).json(res.locals.user);
+});
+
+router.get('/updateDailyStreak', socialController.updateDailyStreak, (req, res) => {
+    res.status(200).json(res.locals.user);
+});
+
+router.get('/updateDailyReset', socialController.updateDailyReset, (req, res) => {
+    res.status(200).json(res.locals.users);
+});
 
 module.exports = router;
