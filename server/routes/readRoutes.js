1c4cc7e9c0693753f3479a798072f01e91f23b91
Charlie Schubach <charlie.schubach@gmail.com>
2024-02-28 11:39:05 -0500

    router updates

--- server/routes/readRoutes.js
+++ server/routes/readRoutes.js
@@ -3,24 +3,36 @@ const readController = require('../controllers/readController');
 
 const router = express.Router();
 
-router.get('/readDailyUser', socialController.readDailyUser, (req, res) => {
+router.get(
+  '/readDailyUser/:username',
+  readController.readDailyUser,
+  (req, res) => {
     res.status(200).json(res.locals.user);
-  });
+  }
+);
 
-router.get('/updateTimeFinished', socialController.updateTimeFinished, (req, res) => {
+router.get(
+  '/updateTimeFinished/:username',
+  readController.updateTimeFinished,
+  (req, res) => {
     res.status(200).json(res.locals.user);
+  }
+);
+
+router.post('/updateTimeSpent', readController.updateTimeSpent, (req, res) => {
+  res.status(200).json(res.locals.user);
 });
 
-router.post('/updateTimeSpent', socialController.updateTimeSpent, (req, res) => {
+router.get(
+  '/updateDailyStreak/:username',
+  readController.updateDailyStreak,
+  (req, res) => {
     res.status(200).json(res.locals.user);
-});
+  }
+);
 
-router.get('/updateDailyStreak', socialController.updateDailyStreak, (req, res) => {
-    res.status(200).json(res.locals.user);
-});
-
-router.get('/updateDailyReset', socialController.updateDailyReset, (req, res) => {
-    res.status(200).json(res.locals.users);
+router.get('/updateDailyReset', readController.updateDailyReset, (req, res) => {
+  res.status(200).json(res.locals.users);
 });
 
 module.exports = router;
