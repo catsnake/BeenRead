f1b76f871e0635c9d8e98c41789ea6947c3e0401
devincarrick <devin.carrick@gmail.com>
2024-02-28 17:55:27 -0500

    Refactor Wikipedia API call in wikipediaController.js

--- server/controllers/wikipediaController.js
+++ server/controllers/wikipediaController.js
@@ -1,21 +1,25 @@
-const axios = require('axios');
+const axios = require("axios");
 
 const wikipediaController = {};
 
 wikipediaController.getArticleOfTheDay = async (req, res, next) => {
   try {
-    const response = await axios.get('https://api.wikimedia.org/feed/v1/wikipedia/en/featured/2024/02/27');
+    const response = await axios.get(
+      "https://api.wikimedia.org/feed/v1/wikipedia/en/featured/2024/02/27"
+    );
     const article = response.data.tfa;
     const { title } = article;
     const description = article.extract;
+
+    console.log("article", article);
 
     res.locals.articleOfTheDay = { title, description, article };
 
     return next();
   } catch (err) {
     return next({
-      log: 'Error in wikipediaController',
-      message: { error: 'Cannot fetch article of the day from Wikipedia' },
+      log: "Error in wikipediaController",
+      message: { error: "Cannot fetch article of the day from Wikipedia" },
     });
   }
 };
