c449a482f7872380b4b7663eb80b954bd2dc400a
Branden Basche <brandenbasche@gmail.com>
2024-02-28 11:19:27 -0500

    tried adding emoji picker component

--- server/server.js
+++ server/server.js
@@ -1,57 +1,57 @@
 const express = require('express');
+
 const app = express();
 const path = require('path');
-const connectDB = require('./config/db');
 const dotenv = require('dotenv');
-const userRouter = require('./routes/userRoutes');
 const cors = require('cors');
+const connectDB = require('./config/db');
+const userRouter = require('./routes/userRoutes');
 const aiController = require('./controllers/openAiController');
 const articleRouter = require('./routes/articleRoutes');
 const socialRouter = require('./routes/socialRoutes');
 const feedRouter = require('./routes/feedRoutes');
+
 const PORT = 3000;
 
-//use dotenv
+// use dotenv
 dotenv.config();
 console.log(process.env.MONGO_URL);
 app.use(express.json());
 
-//allows the server to interact with website
+// allows the server to interact with website
 app.use(cors());
 
-//connect database
+// connect database
 connectDB();
 
 app.use(express.static(path.join(__dirname, '../index.html')));
 
-//use API routers
+// use API routers
 
 app.use('/api/user', userRouter);
 app.use('/api/social', socialRouter);
 app.use('/api/feed', feedRouter);
-//CHAT GPT - add to controller file to modularize
+// CHAT GPT - add to controller file to modularize
 app.get('/api/openai', aiController.getArticle, (req, res) => {
   res.status(200).send(res.locals.getArticle);
 });
 app.use('/api/article', articleRouter);
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-//error handlers for unknown page
-app.use((req, res) =>
-  res.status(404).send("This is not the page you're looking for...")
-);
+// error handlers for unknown page
+app.use((req, res) => res.status(404).send("This is not the page you're looking for..."));
 
-//global error handlers
+// global error handlers
 app.use((err, req, res, next) => {
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
     status: 500,
     message: { err: 'An error occurred' },
   };
-  const errorObj = Object.assign({}, defaultErr, err);
+  const errorObj = { ...defaultErr, ...err };
   // console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj);
 });
