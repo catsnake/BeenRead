3f07b544310642081d9b79cfaa37b585b77fc218
devincarrick <devin.carrick@gmail.com>
2024-02-29 12:51:44 -0500

    Refactor import statements and add console.log statement

--- client/components/ArchiveContainer.jsx
+++ client/components/ArchiveContainer.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import ArchiveItem from './ArchiveItem.jsx';
+import React, { useEffect, useState } from "react";
+import ArchiveItem from "./ArchiveItem.jsx";
 
 const ArchiveContainer = () => {
   const [articles, setArticles] = useState([]);
@@ -9,12 +9,14 @@ const ArchiveContainer = () => {
     const fetchArticles = async () => {
       try {
         const response = await fetch(
-          'http://localhost:3000/api/article/getArchive'
+          "http://localhost:3000/api/article/getArchive"
         );
         const data = await response.json();
+
+        console.log("data:", data[0]); // data: {id: 1, title: "The Best Article", description: "This is the best article.", createdAt: "2021-10-10T00:00:00.000Z", updatedAt: "2021-10-10T00:00:00.000Z"}
         setArticles(data);
       } catch (error) {
-        console.error('Error fetching articles:', error);
+        console.error("Error fetching articles:", error);
       }
     };
 
@@ -22,7 +24,7 @@ const ArchiveContainer = () => {
   }, []);
 
   return (
-    <div className='archive-container'>
+    <div className="archive-container">
       <h1>Archive</h1>
       {articles.map((entry) => (
         <ArchiveItem key={entry.id} article={entry} />
