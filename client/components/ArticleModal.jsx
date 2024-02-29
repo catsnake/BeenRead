d574bbe1051b83a028722d3ea69d938a71fa6b34
Ethan Skalski <ethanskalski@Ethans-MacBook-Pro.local>
2024-02-29 15:52:09 -0500

    charlie

--- client/components/ArticleModal.jsx
+++ client/components/ArticleModal.jsx
@@ -7,6 +7,8 @@ const ArticleModal = ({
   setReadTimes,
   readTimes,
   username,
+  archive,
+  setArchive,
 }) => {
   const [contentText, setContentText] = useState('');
   const contentUrl = 'https://en.wikipedia.org/wiki/Polar_bear';
@@ -15,9 +17,13 @@ const ArticleModal = ({
   const handleModalXBtn = () => {
     console.log('hanle modal x button!');
     setIsModalOpened(false);
-    const tempReadTimes = readTimes;
-    tempReadTimes.push(Date.now());
-    setReadTimes(tempReadTimes);
+    if (!archive) {
+      const tempReadTimes = readTimes;
+      tempReadTimes.push(Date.now());
+      setReadTimes(tempReadTimes);
+    } else {
+      setArchive(false);
+    }
   };
 
   // Logic for when user is done reading article (some back end logic here)
@@ -26,72 +32,76 @@ const ArticleModal = ({
     const URL = 'http://localhost:3000/api/read/';
 
     setIsModalOpened(false);
-    const timeFinished = Date.now();
+    if (!archive) {
+      const timeFinished = Date.now();
 
-    const lastOpen = readTimes[readTimes.length - 1];
-    
-    let timeSpent = 0;
+      const lastOpen = readTimes[readTimes.length - 1];
 
-    for (let i = 0; i < readTimes.length - 1; i += 2) {
-      const openTime = readTimes[i];
-      const closeTime = readTimes[i + 1];
-      timeSpent += closeTime - openTime;
+      let timeSpent = 0;
+
+      for (let i = 0; i < readTimes.length - 1; i += 2) {
+        const openTime = readTimes[i];
+        const closeTime = readTimes[i + 1];
+        timeSpent += closeTime - openTime;
+      }
+
+      timeSpent += timeFinished - lastOpen;
+      timeSpent = Math.floor(timeSpent / 60000); // Convert milliseconds to minutes
+
+      try {
+        await fetch(URL + 'readDailyArticle/' + username, { method: 'PATCH' });
+        await fetch(URL + 'updateTimeFinished/' + username, {
+          method: 'PATCH',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ timeFinished: timeFinished }),
+        });
+        await fetch(URL + 'updateTimeSpent', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ username: username, timeSpent: timeSpent }),
+        });
+      } catch {
+        console.log('Error in read button');
+      }
+
+      setReadTimes([]);
+    } else {
+      setArchive(false);
     }
-
-    timeSpent += timeFinished - lastOpen;
-    timeSpent = Math.floor(timeSpent / 60000); // Convert milliseconds to minutes
-
-    try {
-      await fetch(URL + 'readDailyArticle/' + username, { method: 'PATCH' });
-      await fetch(URL + 'updateTimeFinished/' + username, {
-        method: 'PATCH',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ timeFinished: timeFinished }),
-      });
-      await fetch(URL + 'updateTimeSpent', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username: username, timeSpent: timeSpent }),
-      });
-    } catch {
-      console.log('Error in read button');
-    }
-
-    setReadTimes([]);
   };
 
   // const contentUrl = article.content_urls.desktop.page
 
   return (
-    <div className="article-modal-container">
-      <div className="article-modal-contents">
-        <div className="modal-title-container">
-          <h1 className="modal-title">{article.article.titles.normalized}</h1>
+    <div className='article-modal-container'>
+      <div className='article-modal-contents'>
+        <div className='modal-title-container'>
+          <h1 className='modal-title'>{article.article.titles.normalized}</h1>
           <svg
             onClick={handleModalXBtn}
-            className="modal-x-icon"
-            xmlns="http://www.w3.org/2000/svg"
-            width="200"
-            height="200"
-            viewBox="0 0 24 24"
+            className='modal-x-icon'
+            xmlns='http://www.w3.org/2000/svg'
+            width='200'
+            height='200'
+            viewBox='0 0 24 24'
           >
             <path
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M18 6L6 18M6 6l12 12"
+              fill='none'
+              stroke='currentColor'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              stroke-width='2'
+              d='M18 6L6 18M6 6l12 12'
             />
           </svg>
         </div>
-        <p className="article-body-text">
+        <p className='article-body-text'>
           {article.text}
           {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod illum architecto maxime cupiditate, quibusdam voluptates provident id reprehenderit eius, quisquam veniam ipsam laborum sequi distinctio soluta velit perspiciatis. Minus, pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod illum architecto maxime cupiditate, quibusdam voluptates provident id reprehenderit eius, quisquam veniam ipsam laborum sequi distinctio soluta velit perspiciatis. Minus, pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod illum architecto maxime cupiditate, quibusdam voluptates provident id reprehenderit eius, quisquam veniam ipsam laborum sequi distinctio soluta velit perspiciatis. Minus, pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod illum architecto maxime cupiditate, quibusdam voluptates provident id reprehenderit eius, quisquam veniam ipsam laborum sequi distinctio soluta velit perspiciatis. Minus, pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod illum architecto maxime cupiditate, quibusdam voluptates provident id reprehenderit eius, quisquam veniam ipsam laborum sequi distinctio soluta velit perspiciatis. Minus, pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod illum architecto maxime cupiditate, quibusdam voluptates provident id reprehenderit eius, quisquam veniam ipsam laborum sequi distinctio soluta velit perspiciatis. Minus, pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod illum architecto maxime cupiditate, quibusdam voluptates provident id reprehenderit eius, quisquam veniam ipsam laborum sequi distinctio soluta velit perspiciatis. Minus, pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod illum architecto maxime cupiditate, quibusdam voluptates provident id reprehenderit eius, quisquam veniam ipsam laborum sequi distinctio soluta velit perspiciatis. Minus, pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. */}
         </p>
       </div>
-      <div className="article-modal-button">
-        <a className="done-reading-btn" onClick={handleDoneReadingBtn}>
+      <div className='article-modal-button'>
+        <a className='done-reading-btn' onClick={handleDoneReadingBtn}>
           I'm Done
         </a>
       </div>
